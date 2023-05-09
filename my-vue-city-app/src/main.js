fa590989fd0d7fdfa0d583fbe81bd02681f5305d
kum1416 <kum1416@naver.com>
2023-05-09 13:00:09 +0900

    ss

--- my-vue-city-app/src/main.js
+++ my-vue-city-app/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App';
 // 뷰엑스 스토어 JS 불러오기!
 import store from './store.js';
+// import $ from "jquery";
 
 
 /* 뷰인스턴스 생성하기! */
@@ -27,7 +28,29 @@ new Vue({
     // store.commit('initSet',"https://img.freepik.com/premium-vector/city-illustration_23-2147514701.jpg");
 
     // actions 메서드 호출하기! : dispatch("메서드명",전달값)
-    // store.dispatch('myAct','나야나!');
+    store.dispatch('myAct','나야나!');
 
 }, /////////// created /////
+mounted(){
+  // 링크 클릭시 a에 클래스 on주기
+  // $(".gnb a").click(function(){
+  //     $(this).addClass("on")
+  //     .parent().siblings()
+  //     .find("a").removeClass("on");
+
+  //     // 박스 애니
+  //     showBx();
+  // }); ////////// click ///////////
+
+  // function showBx(){
+  //     // 이미지와 설명박스 순서대로 나타나기
+  //     $("main img")
+  //     .css({opacity:0}).stop()
+  //     .delay(500).fadeTo(500,1);
+
+  //     $("main p")
+  //     .css({opacity:0}).stop()
+  //     .delay(1000).fadeTo(500,1);
+  // } ////// showBx ////
+} ////////// mounted //////
 })
